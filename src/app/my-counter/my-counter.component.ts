22889a1bec29b0754de0a27e00eef42f40136701
BrunnenGH <BrunnenGH@users.noreply.github.com>
2023-02-07 20:47:43 +0100

    priklad s pocitadlem hotovy

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -11,20 +11,22 @@ export class MyCounterComponent {
   count$: Observable<number>;
 
   constructor(private store: Store<{ count: number }>) {
-    this.count$ = store.select('count').pipe(tap((count) => {
-      console.debug(count);
-    }));
+    this.count$ = store.select('count').pipe(
+      tap((count) => {
+        console.debug(count);
+      })
+    );
   }
 
   increment() {
-    // TODO: Dispatch an increment action
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    // TODO: Dispatch a decrement action
+    this.store.dispatch(decrement());
   }
 
   reset() {
-    // TODO: Dispatch a reset action
+    this.store.dispatch(reset());
   }
 }
